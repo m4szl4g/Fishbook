85968d0b766d155957f6e12761705140b24a42c7
Csaba Miklós <mikloscsaba@live.com>
2020-12-22 15:21:16 +0100

    toolbar fix

--- fishbook/src/app/app.module.ts
+++ fishbook/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { HomeComponent } from './home/components/home/home.component';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
@@ -20,7 +18,6 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     SharedModule,
     CoreModule,
     HomeModule,
--- fishbook/src/app/shared/shared.module.ts
+++ fishbook/src/app/shared/shared.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './components/menu/menu.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
     declarations: [
       MenuComponent],
     imports: [
+      BrowserAnimationsModule,
       MatToolbarModule,
+      MatButtonModule,
       MatIconModule
     ],  
     exports: [
+      MenuComponent,
     ],
     providers: [],
     bootstrap: []
