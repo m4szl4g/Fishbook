b686c420258e76bdc5a90438113c77aae1285074
Csaba Miklós <mikloscsaba@live.com>
2021-02-06 07:38:23 +0100

    moved store registrations to core module

--- fishbook/src/app/app.module.ts
+++ fishbook/src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
-import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { MyProfileModule } from './my-profile/my-profile.module';
 import { NewFishModule } from './new-fish/new-fish.module';
-import { AuthModule } from './auth/auth.module';
-import { reducers, metaReducers } from '../app/core/store/index';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,14 +21,6 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
     SharedModule,
     CoreModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, { metaReducers: metaReducers }),
-    StoreRouterConnectingModule.forRoot(),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: !environment.production,
-    }),
-    EffectsModule.forRoot([]),
-
     AuthModule,
     HomeModule,
     MyProfileModule,
--- fishbook/src/app/core/core.module.ts
+++ fishbook/src/app/core/core.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { reducers, metaReducers } from './store/index';
 
 @NgModule({
   declarations: [],
-  imports: [],
+  imports: [
+    StoreModule.forRoot(reducers, { metaReducers: metaReducers }),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !environment.production,
+    }),
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [],
 })
